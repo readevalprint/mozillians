594b5d68e4b0ed45edbabdda7567942e44fa0e35
Tim Watts <tim@readevalprint.com>
2012-05-09 11:40:22 -0700

    replace legend with h2, added style

--- settings/default.py
+++ settings/default.py
@@ -69,6 +69,9 @@ MINIFY_BUNDLES = {
         'test': (
             'css/qunit.css',
         ),
+        'edit_profile': (
+            'css/user.css',
+        ),
     },
     'js': {
         'common': (
